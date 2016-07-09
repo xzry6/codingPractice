5484013b5a675cae8d16cd2f67d24963062a8df4
xzry6 <xzry6@mail.missouri.edu>
2016-07-09 15:30:20 -0700

    dynamic programming solution

--- src/array/RussianDollEnvelopes.java
+++ src/array/RussianDollEnvelopes.java
@@ -5,50 +5,27 @@ import java.util.Comparator;
 
 public class RussianDollEnvelopes {
 
-	int[][] map;
+	public int maxEnvelopes(int[][] envelopes) {
+        if(envelopes == null || envelopes.length == 0 || envelopes[0].length == 0) return 0;
 
-    public int maxEnvelopes(int[][] envelopes) {
-        // Deal with empty and null first.
-        if(envelopes == null || envelopes.length == 0 || envelopes[0].length == 0)
-            return 0;
-
-        int maxWidth = 0;
-        int maxHeight = 0;
-        for(int[] envelope:envelopes) {
-            maxWidth = Math.max(maxWidth, envelope[0]);
-            maxHeight = Math.max(maxHeight, envelope[1]);
-        }
-
-        map = new int[maxWidth + 1][maxHeight + 1];
-
-        // Sort.
         Arrays.sort(envelopes, comparator);
-        // Recursive.
-        
-        return recursive(envelopes, 0, 0, 0);
-    }
 
-    private int recursive(int[][] envelopes, int index, int width, int height) {
-        if(index >= envelopes.length) return 0;
-        int[][] map = this.map;
-        if(map[width][height] != 0) return map[width][height];
+        int[] sum = new int[envelopes.length];
+        int max = 1;
+        Arrays.fill(sum, 1);
 
-        for(int i = index; i < envelopes.length; ++ i) {
-            int curWidth = envelopes[i][0];
-            int curHeight = envelopes[i][1];
-
-            if(curWidth <= width || curHeight <= height) continue;
-
-            int pre = recursive(envelopes, i + 1, curWidth, curHeight) + 1;
-            map[width][height] = Math.max(map[width][height], pre);
+        for(int i = 0; i < envelopes.length; ++ i) {
+            for(int j = 0; j < i; ++ j) {
+                if(envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]) {
+                    sum[i] = Math.max(sum[i], sum[j] + 1);
+                    max = Math.max(max, sum[i]);
+                }
+            }
         }
 
-        return map[width][height];
+        return max;
     }
 
-    /**
-     * Array Comparator.
-     */
     private Comparator<int[]> comparator = new Comparator<int[]>() {
         @Override
         public int compare(int[] a1, int[] a2) {
