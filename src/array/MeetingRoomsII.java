57ad9f175a75ff125bdb39f95c60e08387d9c8e7
xzry6 <xzry6@mail.missouri.edu>
2016-07-19 15:54:26 -0700

    Meeting Rooms II Solution

---
+++ src/array/MeetingRoomsII.java
@@ -0,0 +1,66 @@
+package array;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Given an array of meeting time intervals consisting of 
+ * start and end times [[s1,e1],[s2,e2],...] (si < ei),
+ * find the minimum number of conference rooms required.
+ * 
+ * For example,
+ * Given [[0, 30],[5, 10],[15, 20]],
+ * return 2.
+ * 
+ * @author Sean
+ *
+ */
+public class MeetingRoomsII {
+	
+
+	private Comparator<Interval> comparator = new Comparator<Interval>() {
+        @Override
+        public int compare(Interval i1, Interval i2) {
+            return i1.start - i2.start;
+        }
+    };
+
+    public int minMeetingRooms(Interval[] intervals) {
+        if(intervals == null || intervals.length == 0) return 0;
+
+        Queue<Interval> q1 = new PriorityQueue<Interval>(comparator);
+        Queue<Interval> q2 = new PriorityQueue<Interval>(comparator);
+        int rooms = 0;
+        for(Interval interval:intervals) q1.offer(interval);
+        
+        while(!q1.isEmpty() || !q2.isEmpty()) {
+            Queue<Interval> cur = rooms % 2 == 1 ? q2 : q1;
+            Queue<Interval> next = rooms % 2 == 1 ? q1 : q2;
+            Interval interval = cur.poll();
+            int end = interval.end;
+            rooms ++;
+
+            while(!cur.isEmpty()) {
+                Interval tmp = cur.poll();
+                if(tmp.start < end) next.offer(tmp);
+                else end = tmp.end;
+            }
+        }
+
+        return rooms;
+    }
+
+    public static void main(String[] args) {
+    		MeetingRoomsII mr2 = new MeetingRoomsII();
+    		Interval[] intervals = {new Interval(2, 7)};
+    		System.out.println(mr2.minMeetingRooms(intervals));
+    }
+}
+
+class Interval {
+	int start;
+	int end;
+	Interval() { start = 0; end = 0; }
+	Interval(int s, int e) { start = s; end = e; }
+}
--- src/string/MaximumProductOfWordLengths.java
+++ src/string/MaximumProductOfWordLengths.java
@@ -1,5 +1,53 @@
 package string;
 
-public class MaximumProductOfWordLengths {
+import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * Given a string array words,
+ * find the maximum value of length(word[i]) * length(word[j])
+ * where the two words do not share common letters.
+ * You may assume that each word will contain only lower case letters.
+ * If no such two words exist, return 0.
+ * 
+ * Example 1:
+ * Given ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
+ * Return 16
+ * The two words can be "abcw", "xtfn".
+ * 
+ * Example 2:
+ * Given ["a", "ab", "abc", "d", "cd", "bcd", "abcd"]
+ * Return 4
+ * The two words can be "ab", "cd".
+ * 
+ * Example 3:
+ * Given ["a", "aa", "aaa", "aaaa"]
+ * Return 0
+ * No such pair of words.
+ * 
+ * @author Sean
+ *
+ */
+public class MaximumProductOfWordLengths {
+	public int maxProduct(String[] words) {
+        if(words == null || words.length < 2) return 0;
+
+        int max = 0;
+        for(int i = 0; i < words.length; ++ i) {
+        		Set<Character> set = new HashSet<Character>();
+        		for(int j = 0; j < words[i].length(); ++ j)
+        			set.add(words[i].charAt(j));
+        		for(int k = i + 1; k < words.length; ++ k)
+        			if(!checkDup(set, words[k]))
+        				max = Math.max(max, words[i].length() * words[k].length());
+        }
+
+        return max;
+    }
+
+	private boolean checkDup(Set<Character> set, String word) {
+		for(int i = 0; i < word.length(); ++ i)
+			if(set.contains(word.charAt(i))) return true;
+		return false;
+	}
 }
